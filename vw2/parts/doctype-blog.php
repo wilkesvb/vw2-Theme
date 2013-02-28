55b978ead68abada9291dd8412f576085113e135
Vincent Wilkes <wilkesvb@gmail.com>
2013-02-27 23:29:06 -0500

    IE conditional for Sticky Footer
    Added to both doctype.php

--- vw2/parts/doctype-blog.php
+++ vw2/parts/doctype-blog.php
@@ -5,7 +5,11 @@
   <meta name="viewport" content="width=device-width">
   <title>Vincent Wilkes | Head Music</title>
   <link type="text/css" rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" >
-    
+  <!--[if !IE 7]>
+	<style type="text/css">
+		#wrap {display:table;height:100%}
+	</style>
+  <![endif]-->  
     <?php wp_head(); ?>
 
 </head>
--- vw2/parts/doctype.php
+++ vw2/parts/doctype.php
@@ -1,11 +1,16 @@
 <!Doctype html>
 <html>
 <head>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width">
   <title>Vincent Wilkes | Head Music</title>
   <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-
+  <!--[if !IE 7]>
+	<style type="text/css">
+		#wrap {display:table;height:100%}
+	</style>
+  <![endif]-->
   <?php wp_head(); ?> 
   
 </head>
