7e5de328d1ce05f02fc2b67d125e9dfa397cb56f
Vincent Wilkes <wilkesvb@gmail.com>
2013-02-28 20:09:49 -0500

    Typography Edits and IE fix
    removed 5.5em from icon font pseudo element

---
+++ nav.php
@@ -0,0 +1,5 @@
+<nav class="main-nav">
+  <a class="icon-home" href="http://www.vincentwilkes.com/"><span class="screen-reader-text">Home</span></a>
+  <a class="icon-user" href="http://www.vincentwilkes.com/about"><span class="screen-reader-text">About</span></a>
+  <a class="icon-archive" href="http://www.vincentwilkes.com/archive"><span class="screen-reader-text">Archive</span></a>
+</nav>
